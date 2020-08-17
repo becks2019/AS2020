93d0a94d149991fe69fc1ec7ab00af55fd3c9c9f
Beatrice <beatrice.ins@web.de>
2020-08-17 19:05:44 +0200

    Dialog

---
+++ Raumschiff_Tonstudio/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Dialogue
+{
+    public string name;
+
+
+    [TextArea(3, 10)] //3 ist Minimum an Zeilen vom Textfeld und 10 ist Maximum
+    public string[] sentences;
+}
---
+++ Raumschiff_Tonstudio/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    void OnTriggerEnter(Collider other){
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+    /*public void TriggerDialogue (){
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }*/
+}
