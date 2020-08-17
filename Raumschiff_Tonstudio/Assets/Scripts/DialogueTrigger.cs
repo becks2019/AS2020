5db593780de027b30e2be5e3246fe169c66b72fa
Beatrice <beatrice.ins@web.de>
2020-08-17 19:45:17 +0200

    Dialog 2

--- Raumschiff_Tonstudio/Assets/Scripts/DialogueTrigger.cs
+++ Raumschiff_Tonstudio/Assets/Scripts/DialogueTrigger.cs
@@ -9,6 +9,12 @@ public class DialogueTrigger : MonoBehaviour
     void OnTriggerEnter(Collider other){
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
+
+    void Update(){
+        if(Input.GetKeyDown("space")){
+            FindObjectOfType<DialogueManager>().DisplayNextSentence();
+        }
+    }
     /*public void TriggerDialogue (){
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }*/
