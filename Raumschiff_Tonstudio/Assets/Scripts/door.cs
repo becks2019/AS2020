e833334a282249d93e2a10ee8f25e19b23e9a949
Elisabeth Becker <elisabeth.becker.268@outlook.de>
2020-08-21 16:53:53 +0200

    Skript door für Szenenwechsel

---
+++ Raumschiff_Tonstudio/Assets/Scripts/door.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class door : MonoBehaviour {
+
+    public void OnTriggerEnter(Collider collider) // Trigger deklarieren, der bei Berühung mit Kamera auslöst
+    {
+    SceneManager.LoadScene(2); // Beim Auslösen, lädt hier angegebene Szene
+    }
+
+   
+}
