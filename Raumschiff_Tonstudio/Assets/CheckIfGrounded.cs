7a3c37edc59e5b1ff8bac303fd62dd3d152b75a5
Beatrice <beatrice.ins@web.de>
2020-09-11 23:39:26 +0200

    Footsteps Sounds

---
+++ Raumschiff_Tonstudio/Assets/CheckIfGrounded.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIfGrounded : MonoBehaviour
+{
+
+  public Collider playerCollider;
+  public bool isGrounded; 
+  public bool isOnTerrain; //Terrain Boden
+  public bool isOnCarpet; //Teppich Boden
+  public bool isOnMarmor; //Marmor Boden
+
+  RaycastHit hit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = PlayerGrounded();
+        isOnTerrain = CheckOnTerrain();
+        isOnCarpet = CheckIfCarpet();
+        isOnMarmor = CheckIfMarmor();
+    }
+    
+    //Dieses Skript checkt, ob der Player auf dem Boden steht, indem überprüft wird, ob der Collider des Players den Collider des Terrains/Bodens berührt
+    bool PlayerGrounded(){
+      return Physics.Raycast (transform.position, Vector3.down, out hit, playerCollider.bounds.extents.y + 0.5f);
+    }
+
+    bool CheckOnTerrain(){
+        if(hit.collider != null && hit.collider.tag == "Terrain"){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool CheckIfCarpet(){
+        if(hit.collider != null && hit.collider.tag == "TeppichRot"){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool CheckIfMarmor(){
+        if(hit.collider != null && hit.collider.tag == "Marmor"){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
---
+++ Raumschiff_Tonstudio/Assets/Footsteps.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour
+{
+    public CheckIfGrounded checkIfGrounded;
+
+    public AudioSource audioSource;
+
+    public AudioClip[] stoneClips;
+    public AudioClip[] dirtClips;
+    public AudioClip[] carpetClips;
+    AudioClip previousClip;
+
+    CharacterController character;
+    float currentSpeed;
+    bool walking;
+    float distanceCovered;
+    public float modifier = 0.5f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentSpeed = GetPlayerSpeed();
+        walking = CheckIfWalking();
+
+        if(walking){
+           // distanceCovered += (currentSpeed * Time.deltaTime) * modifier;
+
+            //if(distanceCovered > 1){
+                TriggerNextClip();
+              //  distanceCovered = 0;
+            //}
+        }
+    }
+
+    float GetPlayerSpeed(){
+        float speed = character.velocity.magnitude;
+        return speed;
+    }
+
+    bool CheckIfWalking(){
+        if(currentSpeed > 0 && checkIfGrounded.isGrounded){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    AudioClip GetClipFromArray(AudioClip[] clipArray){
+        int attempts = 3;
+        AudioClip selectedClip = clipArray [Random.Range (0, clipArray.Length - 1)];
+
+        while(selectedClip == previousClip && attempts > 0){
+            selectedClip = clipArray [Random.Range (0, clipArray.Length -1)];
+            attempts--;
+        }
+
+        previousClip = selectedClip;
+        return selectedClip;
+    }
+
+    void TriggerNextClip(){
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.volume = Random.Range(0.8f, 1);
+
+        if(checkIfGrounded.isOnTerrain){
+            audioSource.PlayOneShot (GetClipFromArray (dirtClips), 1);
+        } else if(checkIfGrounded.isOnCarpet){
+            audioSource.PlayOneShot (GetClipFromArray (carpetClips), 1);
+        } else if(checkIfGrounded.isOnMarmor){
+            audioSource.PlayOneShot (GetClipFromArray (stoneClips), 1);
+        }
+    }
+
+}
